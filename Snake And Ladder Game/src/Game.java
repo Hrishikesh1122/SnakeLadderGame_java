b52bcd08d2a0f8e5aaa281d986b4603b3074de6a
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-01 21:15:47 +0530

    Final Commit : UC7 2 player game

--- Snake And Ladder Game/src/Game.java
+++ Snake And Ladder Game/src/Game.java
@@ -3,7 +3,6 @@ public class Game {
 	static final int IS_SNAKE=1;
 	static final int IS_LADDER=2;
 	static final int IS_NOPLAY=3;
-
 	public static int roll() {
 		Random random=new Random();
     	int ran=1+random.nextInt(6);
@@ -15,60 +14,86 @@ public class Game {
     	int opt=1+random.nextInt(3);
     	return opt;	
 	}
- 
+	public static void start() {
+		int posPlayer1=0,posPlayer2=0;
+		int diceCountPlayer1=0,diceCountPlayer2=0;
+		int diceValue=0;
+		int chance=-1;
+		while (true)
+		{
+			diceValue = roll();
+            if (chance == -1)
+            {
+                diceCountPlayer1++;
+                posPlayer1= positionCount(posPlayer1, diceValue);
+                System.out.println("Position of player 1 :"+posPlayer1);
+                if (posPlayer1 == 100)
+                {
+                    System.out.println("Player 1 wins");
+                    System.out.println("Player one rolled dice "+diceCountPlayer1 +" Times");
+                    break;
+                }
+            }
+            else
+            {
+            	 diceCountPlayer2++;
+                 posPlayer2= positionCount(posPlayer2, diceValue);
+                 System.out.println("Position of player 2 :"+posPlayer2);
+                 if (posPlayer2 == 100)
+                 {
+                     System.out.println("Player 2 wins");
+                     System.out.println("Player two rolled dice "+diceCountPlayer2 +" Times");
+                     break;
+                 }
+                
+            }
+            chance = -(chance);
+		}
+	}
+	public static int positionCount(int pos,int diceValue) {
+		 int check;
+        int optionResult=options();
+		switch(optionResult) {
+		case IS_SNAKE:
+			pos-=diceValue;
+			break;
+		case IS_LADDER:
+			pos+=diceValue;
+			System.out.println("Ladder, rolling again");
+			diceValue=roll();
+			check=options();
+			if(check==IS_SNAKE)
+			{
+				pos-=diceValue;
+			}
+			if(check==IS_LADDER)
+			{
+				pos+=diceValue;
+			}
+			if(check==IS_NOPLAY)
+			{
+				pos+=0;
+			}
+			break;
+		case IS_NOPLAY:
+			pos+=0;
+			break;
+		}
+		if(pos<0)
+		{
+			pos=0;
+		} 
+		
+			
+    	if (pos > 100)
+		{
+			pos-=diceValue;		
+		}
+    	return pos;
+	}
+   
 	
 	public static void main(String[] args) {
-		int countdice=0;
-		int pos=0;
-		int diceValue=0;
-		while (pos<=100) {
-			
-			 diceValue=roll();
-			 System.out.println("Dice value is:"+diceValue);
-			 countdice++;
-			int optionResult=options();
-			switch(optionResult) {
-			case IS_SNAKE:
-				pos-=diceValue;
-				System.out.println(pos);
-				break;
-			case IS_LADDER:
-				pos+=diceValue;
-				System.out.println(pos);
-				break;
-			case IS_NOPLAY:
-				pos+=0;
-				System.out.println(pos);
-				break;
-			}
-			if(pos<0)
-			{
-				pos=0;
-				System.out.println(pos);
-			} 
-			if (pos == 100)
-			{
-				System.out.println("Win !!");
-				break;
-			}
-		}	
-		if (pos > 100)
-		{
-			pos-=diceValue;
-		    
-		    while (pos+diceValue!=100)
-		    {
-		    	diceValue=roll();
-		    	System.out.println("Dice value is:"+diceValue);
-		    	countdice++;
-		    }
-		    pos+=diceValue;
-		    System.out.println(pos+" Win !!");
-				
-		}
-		System.out.println("Total rounds played :"+countdice);
-		System.out.println("End of loop Game");
-		
-		
+		start();
 	}
 }
