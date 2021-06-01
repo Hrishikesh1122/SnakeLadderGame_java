271fe308239e0a943c0022dfc02d3d172f0b8c33
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-01 14:18:30 +0530

    Added : UC2 , getting snake,ladder,noplay

--- Snake And Ladder Game/src/Game.java
+++ Snake And Ladder Game/src/Game.java
@@ -1,12 +1,37 @@
 import java.util.Random;
 public class Game {
+	static final int IS_SNAKE=1;
+	static final int IS_LADDER=2;
+	static final int IS_NOPLAY=3;
 
 	public static int roll() {
 		Random random=new Random();
     	int ran=1+random.nextInt(6);
     	return ran;	
 	}
+	
+	public static int options() {
+		Random random=new Random();
+    	int opt=1+random.nextInt(3);
+    	return opt;	
+	}
+	
 	public static void main(String[] args) {
-		roll();
+		int pos=0;
+		int diceValue=roll();
+		int optionResult=options();
+		switch(optionResult) {
+		case IS_SNAKE:
+			pos-=diceValue;
+			break;
+		case IS_LADDER:
+			pos+=diceValue;
+			break;
+		case IS_NOPLAY:
+			pos+=0;
+			break;
+		}
+		System.out.println(pos);
+		
 	}
 }
