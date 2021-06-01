3d1119eab3ac39c0e79dbeb94596107c4255223d
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-01 13:57:57 +0530

    Added UC2: roll()

--- Snake And Ladder Game/src/Game.java
+++ Snake And Ladder Game/src/Game.java
@@ -7,20 +7,6 @@ public class Game {
     	return ran;	
 	}
 	public static void main(String[] args) {
-		int pos =0;
-		while (pos<=100)
-		{
-			pos=pos+roll();
-			System.out.println(pos);
-			if(pos>100)
-				{
-					pos=100;
-					break;
-				}
-		}
-	
-		System.out.println(pos);
-
+		roll();
 	}
-
 }
